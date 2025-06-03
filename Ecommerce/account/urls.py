75b77d894d5bc374abba110de309e6821d2b88f5
AbdulRahman <abdurahmanabdul9@gmail.com>
2025-06-03 22:05:58 +0700

    What I made is the layout of all the html inside the pages, products and acount.So is mean like I do like UI-UX for pages.

--- Ecommerce/account/urls.py
+++ Ecommerce/account/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('login', views.login),
+    path('register', views.register),
+    path('logout', views.logout),
+    path('user_profile', views.user_profile),
 ]
--- Ecommerce/account/views.py
+++ Ecommerce/account/views.py
@@ -4,3 +4,9 @@ from django.shortcuts import render
 
 def login(request):
     return render(request, 'account/login.html')
+def register(request):
+    return render(request, 'account/register.html')
+def logout(request):
+    return render(request, 'account/logout.html')
+def user_profile(request):
+    return render(request, 'account/user_profile.html')
